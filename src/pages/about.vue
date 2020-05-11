41e092f47a87f56c6235b4ab3b3c0a7f744a0abe
sanmu <sanmu@syedu.com>
2020-05-11 16:43:22 +0800

    .

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,3 +3,20 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<!-- mobile -->
+<style lang="scss" scoped>
+  @include mobile-device {
+    h1 {
+      color: yellow;
+    }
+  }
+</style>
+
+<!-- pc -->
+<style lang="scss" scoped>
+  @include desktop-device {
+    h1 {
+      color: green;
+    }
+  }
+</style>
