f1d62493a8cc025859aca393bb325b8efa26bbf4
sanmu <hdy9515@dingtalk.com>
2020-06-08 18:27:27 +0800

    FIX: directive

--- src/utils/directives/element-ui/fixElSelectFilterable.js
+++ src/utils/directives/element-ui/fixElSelectFilterable.js
@@ -1,12 +1,25 @@
 // 解决el-select filterable 移动端focus不弹起软键盘
+import isMobile from '../../libs/isMobile'
 export default {
-  inserted (el) {
+  inserted (el, binding, vnode) {
     const input = el.querySelector('input')
-    if (input) {
-      input.removeAttribute('readOnly')
-      MutationObserver && new MutationObserver((mutations, observer) => {
-        input.removeAttribute('readOnly')
-      }).observe(input, { attributes: true })
+    if (input && isMobile()) {
+      input.removeAttribute('readonly')
+      input.oninput = function (event) {
+        binding.value && vnode.context.$refs[binding.value] && vnode.context.$refs[binding.value].debouncedQueryChange(event)
+      }
+      // input.onblur = () => { // 此方法性能 比 MutationObserver 高
+      //   // 至少要超过200ms, 源码200ms后添加readonly
+      //   setTimeout(() => {
+      //     input.removeAttribute('readonly')
+      //   }, 300)
+      // }
+      el.observer = MutationObserver && new MutationObserver((mutations, observer) => {
+        input.removeAttribute('readonly')
+      }).observe(input, { attributes: true, characterData: true, attributeOldValue: true })
     }
+  },
+  unbind (el) {
+    el.observer && el.observer.disconnect()
   }
 }
