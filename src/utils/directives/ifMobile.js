0422e71290d142e34ae2973cdd1bdee58b09acdd
sanmu <hdy9515@dingtalk.com>
2020-06-08 18:27:27 +0800

    FIX: directive

--- src/utils/directives/ifMobile.js
+++ src/utils/directives/ifMobile.js
@@ -1,9 +1,10 @@
+import isMobile from '../libs/isMobile'
 export default {
   bind (el, binding) {
     const mobileName = binding.value || 'mobile'
     const notMobileName = `not-${mobileName}`
     function check () {
-      if (/Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent)) {
+      if (isMobile()) {
         el.classList.remove(notMobileName)
         el.classList.add(mobileName)
       } else {
--- src/utils/directives/index.js
+++ src/utils/directives/index.js
@@ -1,10 +1,12 @@
 import ifMobile from './ifMobile'
 import ifEventUnself from './ifEventUnself'
 import ifResize from './ifResize'
+import fixElSelectFilterable from './element-ui/fixElSelectFilterable'
 export default {
   install (Vue) {
     Vue.directive('ifMobile', ifMobile)
     Vue.directive('ifEventUnself', ifEventUnself)
     Vue.directive('ifResize', ifResize)
+    Vue.directive('fixElSelectFilterable', fixElSelectFilterable)
   }
 }
